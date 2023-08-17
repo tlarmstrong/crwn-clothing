879dc226ab820f98ab31bdc846fc156e04f1a4db
tracey <tracelyn.armstrong@gmail.com>
2023-08-17 10:46:15 -0500

    add ability to add products to cart and track quantity

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,12 +1,20 @@
 
-import './product-card.styles.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
+
+import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
   console.log(product);
   // destructure off the product object
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`}/>
@@ -17,8 +25,9 @@ const ProductCard = ({ product }) => {
       <Button 
         buttonType="inverted"
         displayText="Add to cart"
-        inputOptions = {{
+        options = {{
           type: "button",
+          onClick: addProductToCart
         }} 
       />
     </div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,7 +15,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  const { isCartOpen } = useContext(CartContext);
+  const { isCartOpen, cartItems } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -36,7 +36,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        { isCartOpen && <CartDropdown /> }
+        { isCartOpen && <CartDropdown items={cartItems} /> }
       </div>
       <Outlet />
     </Fragment>
