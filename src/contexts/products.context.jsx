2f062467b771b7f293a3d43fef5a9b18cada65d9
tracey <tracelyn.armstrong@gmail.com>
2023-10-13 09:29:45 -0500

    one time use function to load items

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,35 @@
-import { createContext, useState } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { createContext, useState, useEffect } from 'react';
+import { 
+  // addCollectionAndDocuments,
+  getCategoriesAndDouments 
+} from '../utils/firebase/firebase.utils';
+
+// initially used to add products
+// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({children}) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // used to add products to the database; one time use
+/*  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA)
+  }, []);*/
+
+  useEffect(() => {
+    // if connecting to async function in useEffect, 
+    // need to wrap call in async function
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDouments();
+      console.log(categoryMap);
+    }
+    getCategoriesMap();
+  }, [])
+
   const value = {products};
   return (
     <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
